e14c91082efb5d4c9d492972589331448bdf051c
cloudyape <ankitjobalerts@gmail.com>
2025-10-18 16:15:28 +0530

    ss init

--- app/not-found.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFoundPage = () => {
-  return (
-   <div className='min-h-screen w-full flex items-center justify-center'>404 Not Found</div>
-  )
-}
-
-export default NotFoundPage
