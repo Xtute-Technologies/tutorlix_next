e32fa65ca747960dbde6e4f41a88a0b29a5582c5
cloudyape <ankitjobalerts@gmail.com>
2025-10-26 14:14:16 +0530

    set tutorlix next

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
 const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "tutorlix";
-  const defaultConfig = singleSpaDefaults({
-    orgName,
-    projectName: "root-config",
-    webpackConfigEnv,
-    argv,
-    disableHtmlGeneration: true,
-  });
+// Export an async function so we can use dynamic import() for ESM-only packages
+module.exports = async (webpackConfigEnv, argv) => {
+  // dynamic import to support ESM-only versions of webpack-config-single-spa
+  let singleSpaDefaultsModule;
+  try {
+    singleSpaDefaultsModule = await import("webpack-config-single-spa");
+  } catch (err) {
+    // fallback to require if import fails for some reason
+    try {
+      // eslint-disable-next-line global-require
+      singleSpaDefaultsModule = require("webpack-config-single-spa");
+    } catch (err2) {
+      console.error(
+        "Failed to load webpack-config-single-spa via import() or require():",
+        err2
+      );
+      throw err2;
+    }
+  }
 
+  const singleSpaDefaults = singleSpaDefaultsModule.default || singleSpaDefaultsModule;
+
+  const orgName = "tutorlix";
+  const defaultConfig = await Promise.resolve(
+    singleSpaDefaults({
+      orgName,
+      projectName: "root-config",
+      webpackConfigEnv,
+      argv,
+      disableHtmlGeneration: true,
+    })
+  );
+
+  // merge and return final config
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
