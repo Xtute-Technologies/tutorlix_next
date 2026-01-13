567b7b1478690d2fe76e3813e40bd7d04f0abb39
devangspsingh <devangshauryapratapsingh@gmail.com>
2026-01-13 18:35:37 +0530

    fix

--- frontend/components/nav-user.jsx
+++ frontend/components/nav-user.jsx
@@ -45,12 +45,12 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user?.profile_image} alt={user.name} />
-                <AvatarFallback className="rounded-lg">{user.name}</AvatarFallback>
+                <AvatarImage src={user?.profile_image} alt={user?.name} />
+                <AvatarFallback className="rounded-lg">{user?.name}</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{user?.name}</span>
+                <span className="truncate text-xs">{user?.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -65,12 +65,12 @@ export function NavUser({
                 <Avatar className="h-8 w-8 rounded-lg">
                   {console.log(user)}
                   
-                  <AvatarImage src={user?.profile_image} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">{user.name}</AvatarFallback>
+                  <AvatarImage src={user?.profile_image} alt={user?.name} />
+                  <AvatarFallback className="rounded-lg">{user?.name}</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{user?.name}</span>
+                  <span className="truncate text-xs">{user?.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
