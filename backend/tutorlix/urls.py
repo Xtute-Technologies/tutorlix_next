98cf188248d699793128ad4e1a013ffadb0f17fe
cloudyape <cloudyape@gmail.com>
2026-04-19 01:10:31 +0530

    added subnav bars

--- backend/tutorlix/urls.py
+++ backend/tutorlix/urls.py
@@ -51,7 +51,9 @@ urlpatterns = [
     path('', include('django_prometheus.urls')),
 ]
 
-# Serve media files in development
+# Serve uploaded media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
