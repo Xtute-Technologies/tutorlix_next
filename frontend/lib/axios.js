39a5c288ab2c1cb84a66c9fdc7ac4a62567d53aa
cloudyape <cloudyape@gmail.com>
2026-04-26 00:24:36 +0530

    added titles page

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = '';
+const API_BASE_URL = 'http://localhost:8000';
 
 // ✅ REMOVE forced Content-Type
 const axiosInstance = axios.create({
